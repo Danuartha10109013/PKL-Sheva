33b8485b034889790445807936d4b017efe8c165
Sheva Virtalioka <virtalsheva@gmail.com>
2024-09-30 21:02:11 +0700

    percobaan

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+namespace App\Http\Controllers;
+
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/latihan',[LatihanC::class,'index'])->name('latihan');
