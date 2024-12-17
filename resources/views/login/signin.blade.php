b1935735604876dc12223644892581d0b2bfbc90
Sheva Virtalioka <odanuartha@gmail.com>
2024-12-17 14:56:46 +0700

    perubhan halaman login

--- resources/views/login/signin.blade.php
+++ resources/views/login/signin.blade.php
@@ -24,7 +24,7 @@
 
 <body class="">
   <div class="container position-sticky z-index-sticky top-0">
-    
+
     </div>
   </div>
   <main class="main-content  mt-0">
@@ -88,15 +88,14 @@
                     </div>
                   </form>
                 </div>
-                
+
               </div>
             </div>
             <div class="col-6 d-lg-flex d-none h-100 my-auto pe-0 position-absolute top-0 end-0 text-center justify-content-center flex-column">
               <div class="position-relative bg-gradient-primary h-100 m-3 px-7 border-radius-lg d-flex flex-column justify-content-center overflow-hidden" style="background-image: url('{{ asset('zen.jpg') }}');
           background-size: cover;">
                 <span class="mask bg-gradient-primary opacity-6"></span>
-                <h4 class="mt-5 text-white font-weight-bolder position-relative">"{{ \Carbon\Carbon::now()->translatedFormat('d F Y') }}"</h4>
-                <p class="text-white position-relative">Today Will Be The Day</p>
+
               </div>
             </div>
           </div>
